52e0b2174b80bf6850db1afd9b220f468eed13fc
Zaveshaa <127344512+zaveshaa@users.noreply.github.com>
2025-02-04 19:35:31 +0300

    create solution.py

---
+++ solution.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+import pygame
+import requests
+
+server_address = 'https://static-maps.yandex.ru/v1?'
+api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+ll_spn = 'll=37.530887,55.703118&spn=0.002,0.002'
+# Готовим запрос.
+
+map_request = f"{server_address}{ll_spn}&apikey={api_key}"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
